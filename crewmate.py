94e2423c17f50c5fef74232c0c60b2209d73cfe4
csully220 <colinsully@msn.com>
2020-10-18 21:54:02 -0500

    Select/deselect tasks

--- crewmate.py
+++ crewmate.py
@@ -9,6 +9,7 @@ class Task:
         self.name = _name
         self.location = _location
         self.completion = _completion
+        self.btn_complete = Checkbox()
 
 class Player:
     step = 10
@@ -50,6 +51,26 @@ class Player:
             surface.blit(self.rightimg,(self.x,self.y)) 
 
 
+class Checkbox():
+    def __init__(self, _checked=False, _x=0, _y=0):
+        self.checked = _checked
+        if(self.checked):
+            self.img = pygame.image.load(r'./data/images/buttons/check.png')
+        else:
+            self.img = pygame.image.load(r'./data/images/buttons/nocheck.png')
+        self.x = _x
+        self.y = _y
+        self.w = 40 
+        self.h = 40
+        self.rect = pygame.Rect(_x, _y, self.w, self.h)
+
+    def draw(self, surface, _checked=False):
+        if _checked:
+            self.img = pygame.image.load(r'./data/images/buttons/check.png')
+        else:
+            self.img = pygame.image.load(r'./data/images/buttons/nocheck.png')
+        surface.blit(self.img,(self.x,self.y))
+
 
 class Button():
     def __init__(self, _action, _x, _y):
@@ -63,12 +84,6 @@ class Button():
 
     def draw(self, surface):
         surface.blit(self.img,(self.x,self.y))
-        
-    def clicked(self,x1,y1,x2,y2,bsize):
-        if x1 >= x2 and x1 <= x2 + bsize:
-            if y1 >= y2 and y1 <= y2 + bsize:
-                return True
-        return False
 
 
 
@@ -88,9 +103,11 @@ class PlayerButton():
 
     def draw(self, surface, chosen=False):
         _font = pygame.font.SysFont('Comic Sans MS', 26)
-        _lbl = _font.render(self.lbl, False, (255, 255, 255))
+        #_lbl = _font.render(self.lbl, False, (255, 255, 255))
+        _color = Color(255, 255, 255)
         if(chosen):
-            _lbl = _font.render(self.lbl, False, (255, 255, 20))
+            _color = Color(255, 255, 20)
+        _lbl = _font.render(self.lbl, False, _color)
         
         surface.blit(_lbl, (self.x,self.y))
         surface.blit(self.img,(self.x,self.y + 36))
@@ -112,6 +129,7 @@ class App:
     bg = pygame.image.load(r'.\data\images\bg_welcome.png')
     players = []
     plyrbtns = []
+    tskbtns = []
     menu = ''
  
     def __init__(self):
@@ -133,7 +151,7 @@ class App:
             for _t in _xmltasks:
                 _tn = _t.get('name')
                 _tl = _t.get('location')
-                _tc = _t.get('completion')
+                _tc = int(_t.get('completion'))
                 tasks.append(Task(_tn, _tl, _tc))
             new_plyr.tasks = tasks
             self.players[new_plyr.name] = new_plyr
@@ -180,38 +198,56 @@ class App:
             self.display_surf.blit(self.bg, (0, 0))
             lbl_plyrsel = self.myfont.render('Select Player', False, (255, 255, 255))
             self.display_surf.blit(lbl_plyrsel, (60,30))
+            lbl_plyrsel = self.myfont.render('Crewmate IRL Task Simulator', False, (255, 255, 255))
+            self.display_surf.blit(lbl_plyrsel, (320,220))
             #self.player.draw(self.display_surf)
             pbx = 60
             pby = 100
             yofs = 0
+            i = 0
             for pn, po in self.players.items():
                 #if pn != 'Common':
                 _btn = po.btn_choose
+                if i == 4:
+                    pby = 100
+                    pbx = 150
+                    yofs = 0
                 _btn.x = pbx
                 _btn.y = pby + yofs
                 yofs = yofs + 94
                 _btn.draw(self.display_surf, po.chosen)
+                i += 1;
 
             self.btn_plyrsel.draw(self.display_surf)
             self.btn_tasks.draw(self.display_surf)
             
-        if self.menu == 'TASKS':
-                self.display_surf.blit(self.bg, (0, 0))
-                _font = pygame.font.SysFont('Comic Sans MS', 26)
-
-                _lbl = _font.render('Tasks:' , False, (255, 255, 255))
-                self.display_surf.blit(_lbl, (60,60))
-                    
-                _x = 60
-                _y = 100
-                _yofs = 40
-                for t in self.player.tasks:
-                    #print(t)
-                    _strtsk = t.location + ' - ' + t.name + '. Complete: ' + t.completion
-                    _lbl = _font.render(_strtsk , False, (255, 255, 255))
-                    self.display_surf.blit(_lbl, (_x,_y))
-                    _y = _y + _yofs
-            
+        elif self.menu == 'TASKS':
+            self.tskbtns.clear()
+            self.display_surf.blit(self.bg, (0, 0))
+            _font = pygame.font.SysFont('Comic Sans MS', 26)
+            _lbl = _font.render('Your Tasks:' , False, (255, 255, 255))
+            self.display_surf.blit(_lbl, (60,60))
+                
+            _x = 90
+            _y = 100
+            _yofs = 40
+            # Player tasks
+            for t in self.player.tasks:
+                #print(t)
+                _strtsk = t.location + ' - ' + t.name
+                _color = Color(255, 255, 255)
+                if t.completion:
+                    _color = Color(255, 255, 20)
+                _lbl = _font.render(_strtsk , False, _color)
+                self.display_surf.blit(_lbl, (_x,_y))
+                
+                _chkbx = t.btn_complete
+                _chkbx.x = _x - 30
+                _chkbx.y = _y + 12
+                _chkbx.draw(self.display_surf, t.completion)
+                self.tskbtns.append(_chkbx)
+                _y = _y + _yofs
+  
         pygame.display.flip()
 
 
@@ -230,37 +266,53 @@ class App:
                 # handle MOUSEBUTTONUP
                 if event.type == pygame.MOUSEBUTTONUP:
                     mspos = pygame.mouse.get_pos()
-                    # CHOOSE PLAYER
-                    _plyrclkd = False
-                    for pn, po in self.players.items():
-                        if po.btn_choose.get_rect().collidepoint(mspos):
-                            print('Chose ' + pn)
-                            self.player = po
-                            _plyrclkd = True
-                            po.chosen = True
-                    if(_plyrclkd):
+                    if self.menu == 'WELCOME':
+                        # CHOOSE PLAYER
                         _plyrclkd = False
                         for pn, po in self.players.items():
-                            po.chosen = False
-                            if po.name == self.player.name:
+                            if po.btn_choose.get_rect().collidepoint(mspos):
+                                print('Chose ' + pn)
+                                self.player = po
+                                _plyrclkd = True
                                 po.chosen = True
-                    # SELECT
-                    if self.btn_plyrsel.rect.collidepoint(mspos):
-                        print('Click!')
-                        self.menu = 'TASKS'
-                        self.bg = pygame.image.load(r'.\data\images\bg_sparse.png')
-                    # TASKS
-                    if self.btn_tasks.rect.collidepoint(mspos):
-                        self.menu = 'TASKS'
-                        self.bg = pygame.image.load(r'.\data\images\bg_sparse.png')
-                    # EXIT
-                    exit_rect = pygame.Rect(76, 540, 100, 50)
-                    if exit_rect.collidepoint(mspos):
-                        if self.menu == 'TASKS':
-                            self.menu = 'WELCOME'
-                        elif self.menu == 'WELCOME':
+                        if(_plyrclkd):
+                            _plyrclkd = False
+                            for pn, po in self.players.items():
+                                po.chosen = False
+                                if po.name == self.player.name:
+                                    po.chosen = True
+                        # SELECT
+                        if self.btn_plyrsel.rect.collidepoint(mspos):
+                            print('Click!')
+                            self.menu = 'TASKS'
+                            self.bg = pygame.image.load(r'.\data\images\bg_sparse.png')
+                        # TASKS
+                        if self.btn_tasks.rect.collidepoint(mspos):
+                            self.menu = 'TASKS'
+                            self.bg = pygame.image.load(r'.\data\images\bg_sparse.png')
+                        # EXIT
+                        exit_rect = pygame.Rect(76, 540, 100, 50)
+                        if exit_rect.collidepoint(mspos):
                             print('Exit!')
                             self.running = False
+                            
+                    elif self.menu == 'TASKS':
+                        idx = 0
+                        for tb in self.tskbtns:
+                            _tbrect = pygame.Rect(tb.x, tb.y, tb.w, tb.h)
+                            if _tbrect.collidepoint(mspos):
+                                if self.player.tasks[idx].completion > 0:
+                                    self.player.tasks[idx].completion = 0
+                                    print('Task incomplete')
+                                elif self.player.tasks[idx].completion == 0:
+                                    self.player.tasks[idx].completion = 1
+                                    print('Task complete')
+                            idx += 1
+                        # EXIT
+                        exit_rect = pygame.Rect(76, 540, 100, 50)
+                        if exit_rect.collidepoint(mspos):
+                            self.menu = 'WELCOME'
+                            self.bg = pygame.image.load(r'.\data\images\bg_welcome.png')
                     print(mspos)
 
             pygame.event.pump()
